1021afd1402e4e3df37ffddd0974c159fd279800
Seva Zakharchenko <vseva80@yandex.ru>
2019-04-09 15:50:23 +0300

    Add project files.

---
+++ SimpleHeartbeatService/HeartBeat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SimpleHeartbeatService
+{
+    public class HeartBeat
+    {
+        //this is the complete service
+
+        private readonly Timer _timer;
+
+        public HeartBeat()
+        {
+            //deffine event end fow often this event fired
+            _timer = new Timer(1000) { AutoReset = true }; 
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //get current time && append time all the way to the end of file
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"Heartbeat.txt", lines);
+        }
+
+        //give chance through static interface to timer
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+    }
+}
---
+++ SimpleHeartbeatService/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SimpleHeartbeatService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+                {
+                    x.Service<HeartBeat>(s =>   //setUp service pattern
+                    {
+                        s.ConstructUsing(heartbeat => new HeartBeat());
+                        s.WhenStarted(heartbeat => heartbeat.Start());
+                        s.WhenStopped(heartbeat => heartbeat.Stop());
+                    });
+                    
+                    //set name + description for service
+                    x.RunAsLocalSystem();
+                    x.SetServiceName("HeartBeatServise");
+                    x.SetDisplayName("HeartBeatServise");
+                    x.SetDescription("HeartBeatServiseHeartBeatServiseHeartBeatServise");
+                });
+
+            //covert enum TopshelfExitCode to int and pass it out
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+
+        }
+    }
+}
+
+//[TODO] crete service that clean Temp folder
+//
